62e4e6756f568a0a488d1ebacf4e0f53311ff5be
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2021-03-12 02:05:40 +0530

    making it responsive

--- src/app/components/Sidebar/Sidebar.js
+++ src/app/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Container, Form, Button, CloseButton, Card } from 'react-bootstrap';
-import Checkbox from '../Checkbox/checkbox'
+import Checkbox from '../Checkbox/checkbox';
 import './sidebar.scss';
 
 const Sidebar = ({ setShowSidebar }) => {
@@ -17,41 +17,49 @@ const Sidebar = ({ setShowSidebar }) => {
 						<div className='mb-3'>
 							<Card className='sidebar__card py-3 pr-3'>
 								<h4 className='mb-3 text-black'>Project Types</h4>
-								<Checkbox label="Formal"></Checkbox>
-								<Checkbox label="Informal"></Checkbox>
+								<Checkbox label='Formal'></Checkbox>
+								<Checkbox label='Informal'></Checkbox>
 							</Card>
 						</div>
 						<div className='mb-3'>
 							<Card className='sidebar__card py-3 pr-3'>
 								<h4 className='mb-3 text-black'>Course Types</h4>
-								<Checkbox label="LOP"></Checkbox>
-								<Checkbox label="DOP"></Checkbox>
-								<Checkbox label="SOP"></Checkbox>
+								<Checkbox label='LOP'></Checkbox>
+								<Checkbox label='DOP'></Checkbox>
+								<Checkbox label='SOP'></Checkbox>
 							</Card>
 						</div>
 						<div className='mb-3'>
 							<Card className='sidebar__card py-3 pr-3'>
 								<h4 className='mb-3 text-black'>Departments</h4>
-								<div class="row">
-									<div class="col-sm-6">
-										<Checkbox label="Checmical"></Checkbox>
-										<Checkbox label="Pharmacy"></Checkbox>
-										<Checkbox label="Economics"></Checkbox>
-										<Checkbox label="Biology"></Checkbox>
-										<Checkbox label="Computer Science"></Checkbox>
-										<Checkbox label="Mechanical &amp; Manufcaturing"></Checkbox>
+								<div class='row'>
+									<div class='col-sm-6'>
+										<Checkbox label='Chemical'></Checkbox>
+										<Checkbox label='Pharmacy'></Checkbox>
+										<Checkbox label='Economics'></Checkbox>
+										<Checkbox label='Biology'></Checkbox>
+										<Checkbox label='Computer Science'></Checkbox>
+										<Checkbox label='Mechanical &amp; Manufcaturing'></Checkbox>
 									</div>
-									<div class="col-sm-6">
-										<Checkbox label="Phoenix"></Checkbox>
-										<Checkbox label="Math"></Checkbox>
-										<Checkbox label="Civil"></Checkbox>
+									<div class='col-sm-6'>
+										<Checkbox label='Phoenix'></Checkbox>
+										<Checkbox label='Math'></Checkbox>
+										<Checkbox label='Civil'></Checkbox>
 									</div>
 								</div>
 							</Card>
 						</div>
 						<Card className='sidebar__card py-4 px-2 b-0'>
-								<Button variant="outline-primary" className="btn-block btn-outline">Apply Filter</Button>
-								<Button variant='outline-danger' className='my-2 btn-block btn-outline'>Reset Filter</Button>
+							<Button
+								variant='outline-primary'
+								className='btn-block btn-outline'>
+								Apply Filter
+							</Button>
+							<Button
+								variant='outline-danger'
+								className='my-2 btn-block btn-outline'>
+								Reset Filter
+							</Button>
 						</Card>
 					</Form>
 				</Container>
